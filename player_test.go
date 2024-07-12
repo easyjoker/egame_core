653d240f2be4356ae40fd0405d55c451255e1501
easyjoker <easyjoker@gmail.com>
2024-07-12 18:58:54 +0800

    遷移 player_test 到本專案

---
+++ player_test.go
@@ -0,0 +1,28 @@
+package egamecore
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+// 測試玩家的行為
+func TestNewPlayer(t *testing.T) {
+	player := Player{Id: 1, Name: "test", Balance: 100}
+
+	marshed, err := json.Marshal(player)
+
+	if err != nil {
+		t.Errorf("Player marshed has error")
+	}
+
+	var restored Player
+	err = json.Unmarshal(marshed, &restored)
+	if err != nil {
+		t.Errorf("Player unmarshed is not correct")
+	}
+
+	if !reflect.DeepEqual(player, restored) {
+		t.Errorf("Player unmarshed is not the same as original player")
+	}
+}
